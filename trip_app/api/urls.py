12843caf2a6f8f8ccb7536dd6bd61650328c5108
sirblacksnow <domezaza48@gmail.com>
2023-09-19 21:30:55 +0700

    api checkIn

--- trip_app/api/urls.py
+++ trip_app/api/urls.py
@@ -12,11 +12,7 @@ urlpatterns = [
     path('trip-all-join/', get_list_trip_all, name='tripView-all'),
     path('trip-all-join/<int:pk>/', get_list_trip_all_user, name='tripView-all'),
     
-    path('trip-detail-id-all/<int:id>/', TripDetailIdViewAV.as_view(), name='tripDetailIdView-all'),
-    path('trip-detail-id/<int:id_trip>/<int:id_place>/', TripDetailPlaceAndTripIdViewAV.as_view(), name='tripPlaceAndTripIdView-all'),
-    
     path('trip/', TripSearchView.as_view(), name='tripView'),
-    path('trip/<int:id>/', TripDetailViewAV.as_view(), name='tripDetailView'),
     
     path('trip-user/<str:username>/', TripUserViewAV.as_view(), name='tripUserView'),
     path('trip-user2/<int:pk>/', get_trip_user, name='tripUser2View'),
--- trip_app/api/views.py
+++ trip_app/api/views.py
@@ -116,32 +116,6 @@ class TripSearchView(generics.ListAPIView):
     filter_backends = [filters.SearchFilter]
     search_fields = ['name']
     
-class TripDetailViewAV(APIView):
-    
-    serializer_class = TripSerializer
-
-    def get(self, request, id):
-        try:
-            trip = Trip.objects.get(id=id)
-        except Trip.DoesNotExist :
-            return Response({'error': 'Trip not found'}, status=status.HTTP_404_NOT_FOUND)
-        serializer = TripSerializer(trip)
-        return Response(serializer.data)
-
-    def put(self, request, id):
-        trip = Trip.objects.get(id=id)
-        serializer = TripSerializer(trip, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, id):
-        trip = Trip.objects.get(id=id)
-        trip.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
 #### Detail Place List in Trip
 
 def get_list_trip_all(request):
@@ -479,27 +453,3 @@ class TripUserIdViewAV(generics.ListAPIView):
         id = self.kwargs['id']
         return Trip.objects.filter(user_id=id)
     
-class TripDetailIdViewAV(generics.ListAPIView):
-    
-    serializer_class = TripDetailSerializer
-
-    def get_queryset(self):
-        id = self.kwargs['id']
-        return TripDetail.objects.filter(trip__id=id)
-    
-class TripDetailPlaceAndTripIdViewAV(generics.ListAPIView):
-    
-    serializer_class = TripDetailSerializer
-
-    def get_queryset(self):
-        id_trip = self.kwargs['id_trip']
-        id_place = self.kwargs['id_place']
-        return TripDetail.objects.filter(trip__id=id_trip).filter(place__id=id_place)
-    
-# class TripUserAllViewAV(generics.ListAPIView):
-    
-#     serializer_class = TripSerializer
-
-#     def get_queryset(self):
-#         id = self.kwargs['id']
-#         return Trip.objects.filter(user_id=id)
