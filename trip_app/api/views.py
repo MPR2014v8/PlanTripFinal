b820436a9ac0f41ef9867c2493fb694fada577f7
sirblacksnow <domezaza48@gmail.com>
2023-09-19 21:46:55 +0700

    api checkIn

--- trip_app/api/views.py
+++ trip_app/api/views.py
@@ -34,7 +34,7 @@ def get_count_trip_detail(request, pk):
         cursor.execute(sql)
         data_read = cursor.fetchall()
 
-    if data_read is None or len(data_read) <= 0:
+    if data_read is None or len(data_read) == 0:
         data_list.append({
             "count_trip_detail": "0",
             "trip_id": str(pk),
@@ -110,11 +110,46 @@ class TripViewAV(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+    def put(self, request, id):
+        trip = Trip.objects.get(id=id)
+        serializer = TripSerializer(trip, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 class TripSearchView(generics.ListAPIView):
     queryset = Trip.objects.all()
     serializer_class = TripSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['name']
+    
+class TripDetailViewAV(APIView):
+    
+    serializer_class = TripSerializer
+
+    def get(self, request, id):
+        try:
+            trip = Trip.objects.get(id=id)
+        except Trip.DoesNotExist :
+            return Response({'error': 'Trip not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = TripSerializer(trip)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        trip = Trip.objects.get(id=id)
+        serializer = TripSerializer(trip, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        trip = Trip.objects.get(id=id)
+        trip.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
     
 #### Detail Place List in Trip
 
@@ -453,3 +488,27 @@ class TripUserIdViewAV(generics.ListAPIView):
         id = self.kwargs['id']
         return Trip.objects.filter(user_id=id)
     
+class TripDetailIdViewAV(generics.ListAPIView):
+    
+    serializer_class = TripDetailSerializer
+
+    def get_queryset(self):
+        id = self.kwargs['id']
+        return TripDetail.objects.filter(trip__id=id)
+    
+class TripDetailPlaceAndTripIdViewAV(generics.ListAPIView):
+    
+    serializer_class = TripDetailSerializer
+
+    def get_queryset(self):
+        id_trip = self.kwargs['id_trip']
+        id_place = self.kwargs['id_place']
+        return TripDetail.objects.filter(trip__id=id_trip).filter(place__id=id_place)
+    
+# class TripUserAllViewAV(generics.ListAPIView):
+    
+#     serializer_class = TripSerializer
+
+#     def get_queryset(self):
+#         id = self.kwargs['id']
+#         return Trip.objects.filter(user_id=id)
